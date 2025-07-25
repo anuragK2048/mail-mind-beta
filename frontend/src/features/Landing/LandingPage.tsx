acb480fdf771f5a4b6ec855af17d7e396401f1d0
anuragK2048 <developeranurag111@gmail.com>
2025-07-25 10:29:35 +0530

    testing

--- frontend/src/features/Landing/LandingPage.tsx
+++ frontend/src/features/Landing/LandingPage.tsx
@@ -4,10 +4,10 @@ import { useLocation } from "react-router";
 function LandingPage() {
   const location = useLocation();
   console.log(location.state?.from?.pathname);
-
+  const temp = import.meta.env.VITE_API_BASE_URL;
   return (
     <div>
-      <button onClick={handleLogin}>Login/Signup</button>
+      <button onClick={handleLogin}>{temp}</button>
     </div>
   );
 }
